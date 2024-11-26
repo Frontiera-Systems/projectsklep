9bd4d49f61f4259f757793cf492c54943059567d
mfrontiera <michal.frontiera@gmail.com>
2024-11-26 18:50:04 +0100

    Update SecurityConfiguration.java

--- src/main/java/com/example/application/views/mainview/security/SecurityConfiguration.java
+++ src/main/java/com/example/application/views/mainview/security/SecurityConfiguration.java
@@ -6,10 +6,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @EnableWebSecurity
@@ -19,13 +21,30 @@ public class SecurityConfiguration extends VaadinWebSecurity {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         // http.rememberMe().alwaysRemember(false);
-        // super.configure(HttpSecurity) as it adds final anyRequest matcher
 
-        http.authorizeHttpRequests(auth -> auth.requestMatchers(new AntPathRequestMatcher("/public/**")).permitAll());
+        // http.authorizeHttpRequests(auth -> auth.requestMatchers(new AntPathRequestMatcher("/public/**")).permitAll());
+        http.authorizeHttpRequests((authz) -> authz
+                .requestMatchers(
+                        new AntPathRequestMatcher("/h2-console/**")
+                ).hasRole("ADMIN")
+        );
 
-        super.configure(http);
+        http.csrf((csrf) ->
+                csrf.ignoringRequestMatchers(
+                        new AntPathRequestMatcher("/h2-console/**")
+                ).csrfTokenRepository(
+                        CookieCsrfTokenRepository.withHttpOnlyFalse()
+                )
+        );
+        http.headers((headers) -> headers
+                .frameOptions(
+                        HeadersConfigurer.FrameOptionsConfig::disable
+                )
+        );
 
         setLoginView(http, LoginView.class);
+        super.configure(http);
+
     }
 
     @Override
