f52e694647c7b7406e53fb204827e60030361505
mfrontiera <michal.frontiera@gmail.com>
2024-11-26 13:39:30 +0100

    Update SecurityConfiguration.java

--- src/main/java/com/example/application/views/mainview/security/SecurityConfiguration.java
+++ src/main/java/com/example/application/views/mainview/security/SecurityConfiguration.java
@@ -18,27 +18,13 @@ public class SecurityConfiguration extends VaadinWebSecurity {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        // Delegating the responsibility of general configurations
-        // of http security to the super class. It's configuring
-        // the followings: Vaadin's CSRF protection by ignoring
-        // framework's internal requests, default request cache,
-        // ignoring public views annotated with @AnonymousAllowed,
-        // restricting access to other views/endpoints, and enabling
-        // NavigationAccessControl authorization.
-        // You can add any possible extra configurations of your own
-        // here (the following is just an example):
-
         // http.rememberMe().alwaysRemember(false);
-
-        // Configure your static resources with public access before calling
         // super.configure(HttpSecurity) as it adds final anyRequest matcher
         http.authorizeHttpRequests(auth -> auth.requestMatchers(new AntPathRequestMatcher("/public/**"))
                 .permitAll());
 
         super.configure(http);
 
-        // This is important to register your login view to the
-        // navigation access control mechanism:
         setLoginView(http, LoginView.class);
     }
 
@@ -48,11 +34,8 @@ public class SecurityConfiguration extends VaadinWebSecurity {
         super.configure(web);
     }
 
-    /**
-     * Demo UserDetailsManager which only provides two hardcoded
-     * in memory users and their roles.
-     * NOTE: This shouldn't be used in real world applications.
-     */
+    
+
     @Bean
     public UserDetailsManager userDetailsService() {
         UserDetails user =
